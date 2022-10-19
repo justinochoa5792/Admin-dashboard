225ef3c63dc4fb2a20ba07cd05d03f11d8efd595
Justin Ochoa <just.och92@gmail.com>
2022-10-19 14:03:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
+import { Dashboard } from "@mui/icons-material";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+            <TopBar />
+            <h1>Hello World</h1>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
